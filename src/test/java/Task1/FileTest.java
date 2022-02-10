a6755d9dbafd217e6fb1e1bbc78187e058188491
MarthaKoltinova <96679318+MarthaKoltinova@users.noreply.github.com>
2022-02-10 16:37:54 +0300

    Add files via upload

--- src/test/java/Task1/FileTest.java
+++ src/test/java/Task1/FileTest.java
@@ -12,11 +12,12 @@ public class FileTest {
     public static void main(String[] args) throws IOException {
         Path dir = Paths.get("src/test/java/Task1/testDirectory");
         String extension = ".txt";
+        int MAX = 2;
         List<String> result = new ArrayList<>();
-        //поиск
+        //
         List<Path> files = Files.list(dir).filter(data -> data.getFileName().toString().contains(extension)).collect(Collectors.toList());
         if (files.size() != 0) {
-            //последний созданный файл
+            //
             Path lastCreated = files.stream().max((f1, f2) -> Long.compare(f1.toFile().lastModified(), f2.toFile().lastModified())).get();
             Long lastCreatedSeconds = Files.readAttributes(lastCreated, BasicFileAttributes.class).creationTime().to(TimeUnit.SECONDS);
             result.add(lastCreated.toFile().getName());
@@ -24,7 +25,7 @@ public class FileTest {
             //Проверяю файл созданные не более 10 сек назад
             for (Path file : files) {
                 try {
-
+                    //
                     Long fileCreationSeconds = Files.readAttributes(file, BasicFileAttributes.class).creationTime().to(TimeUnit.SECONDS);
                     if (lastCreatedSeconds - fileCreationSeconds <= 10) {
                         result.add(file.getFileName().toString());
@@ -34,8 +35,15 @@ public class FileTest {
                 }
             }
         }
-        //
-        System.out.println(String.join(", ", result));
+        if (MAX == 0) {
+            System.out.println(String.join(", ", result));
+        } else {
+            List<String> cropList = new ArrayList<>();
+            for (int index = 0; index < MAX; index++) {
+                cropList.add(result.get(index));
+            }
+            System.out.println(String.join(", ", cropList));
+        }
     }
 }
 
